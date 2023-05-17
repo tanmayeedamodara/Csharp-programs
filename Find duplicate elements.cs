8cd9bdbce23d0c50f831ab7752c04275e867a8d3
tanmayeedamodara <133097423+tanmayeedamodara@users.noreply.github.com>
2023-05-17 18:07:09 +0530

    Create Find duplicate elements.cs

---
+++ Find duplicate elements.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class FindDuplicateElements
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 1, 3, 4, 2, 2, 5, 5, 5, 3, 5 };
+            Dictionary<int, int> dup = new Dictionary<int, int>();
+
+            foreach(int a in arr)
+            {
+                if(dup.ContainsKey(a))
+                {
+                    dup[a]++;
+                }
+                else
+                {
+                    dup[a] = 1;
+                }
+            }
+            int noOfDupElements = 0;
+            foreach(KeyValuePair<int, int> num in dup)
+            {
+                if (num.Value > 1)
+                {
+                    Console.WriteLine($"Element {num.Key} occurs {num.Value}");
+                    noOfDupElements++;
+                }
+            }
+            Console.WriteLine($"No. of duplicate elements {noOfDupElements}");
+
+            Console.ReadLine();
+        }
+    }
+}
